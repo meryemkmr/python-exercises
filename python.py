c3e679796246dc90a3edc0d06a9bbc479d665c09
meryemkmr <meryemkmr36@gmail.com>
2020-01-14 16:28:52 -0600

    Madlib

--- python.py
+++ python.py
@@ -3,6 +3,11 @@
 # output = f'hello , {name} '
 # print(output)
 
-name = input ('what is your name?')
-message = f' Hello {name}! \n Your Name Has 6 Letter In It! Awesome!'
-print(message)
+# name = input ('what is your name?')
+# message = f' Hello {name}! \n Your Name Has 6 Letter In It! Awesome!'
+# print(message) # hello you!
+
+name ="Meryem"
+subject = "Python"
+msg = f"{name}'s favorite subject in scholl is {subject}."
+print(msg)  
