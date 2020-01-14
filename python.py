535be7b916d9545e095e2a7088f1063769326756
meryemkmr <meryemkmr36@gmail.com>
2020-01-14 17:01:21 -0600

    Day of Week

--- python.py
+++ python.py
@@ -5,9 +5,31 @@
 
 # name = input ('what is your name?')
 # message = f' Hello {name}! \n Your Name Has 6 Letter In It! Awesome!'
-# print(message) # hello you!
+# print(message.upper()) # hello you!
 
-name ="Meryem"
-subject = "Python"
-msg = f"{name}'s favorite subject in scholl is {subject}."
-print(msg)  
+# name =input("What is your name?")
+# subject = input("What is subject")
+# msg = f"{name}'s favorite subject in scholl is {subject}."
+# print(msg)  # Madlib
+
+
+# day = int(input('Day (0-6)?'))
+# print(day)
+
+day_of_week = int(input('Day (0-6)?'))
+if (day_of_week == 0):
+    print('sunday')
+elif(day_of_week == 1 ):
+    print('monday') 
+elif(day_of_week == 2 ):
+    print('thuesday')  
+elif(day_of_week == 3 ):
+    print('wednesday') 
+elif(day_of_week == 4 ):
+    print('thusday') 
+elif(day_of_week == 5 ):
+    print('friday')  
+elif(day_of_week == 6 ):
+    print('saturday') 
+else :
+    print('return to  page')        
