f2da7b41838634864956e39d573749d792be8c5f
meryemkmr <meryemkmr36@gmail.com>
2020-01-16 17:51:22 -0600

    hw

---
+++ 4d_hw_medium.py
@@ -0,0 +1,33 @@
+# Small
+#1. Madlib function
+
+# def madlib(name,subject):
+#     return(f"{name}'s favorite subject is {subject}")
+    
+
+# madlib1 = madlib('Jenn','Science')  
+# print(madlib1)  
+
+
+# #2. Celsius to Fahrenheit conversion
+
+# def convert(Celcius):
+#     return (Celcius*9/5+32)
+# temp = convert(0)
+# print(temp)
+
+#3. Fahrenheit to Celsius conversion
+# def convert(Fahrenheit):
+#     return (Fahrenheit-32*5/9)
+# temp = convert(100)
+# print(temp)
+
+#4. is_even function
+
+def is_even(num):
+    if num % 2 == 0:
+        print('even')
+    else:
+        print('odd')
+even_num = is_even(9)        
+print(even_num)
