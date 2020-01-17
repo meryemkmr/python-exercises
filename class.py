e486c082b0a9e944fb790ee4e1772125d06f29af
meryemkmr <meryemkmr36@gmail.com>
2020-01-17 13:20:44 -0600

    phone book app

--- class.py
+++ class.py
@@ -210,25 +210,25 @@
 # print(contact[1])
 # print(contact[1]['last_name'],contact[2]['first_name'])
 # print(contact[0]['phone']['cell'])
-contact = [
-    {
-        'first_name': 'Phillip',
-        'last_name': 'Guo',
-        'email': 'phillip.guo@gmail.com',
-        'phone':{
-            'work':'837-494-3948',
-            'cell': '234-897-4933'
-        }
-    },
-    {
-        'first_name': 'Mark',
-        'last_name': 'Guzdial',
-        'email': 'mark.guzdial@gatech.edu',
-        'phone':{
-            'work':'484-569-3466',
-            'cell': '493-485-9845'
-        }
-    }
-]
+# contact = [
+#     {
+#         'first_name': 'Phillip',
+#         'last_name': 'Guo',
+#         'email': 'phillip.guo@gmail.com',
+#         'phone':{
+#             'work':'837-494-3948',
+#             'cell': '234-897-4933'
+#         }
+#     },
+#     {
+#         'first_name': 'Mark',
+#         'last_name': 'Guzdial',
+#         'email': 'mark.guzdial@gatech.edu',
+#         'phone':{
+#             'work':'484-569-3466',
+#             'cell': '493-485-9845'
+#         }
+#     }
+# ]
 
-print(contact[1]['phone']['work'])
+# print(contact[1]['phone']['work'])
